25942d377268c25a0c48d991eaa6e0378c97598e
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-07-01 00:23:13 -0400

    Feat: Se agrega js y scss dando ultimos acabados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,33 @@
-var1 = Number(prompt("ingresa el numero"))
-var2 = Number(prompt("Ingresa otro numero"))
+// Verifica que la pagina cargue
+window.onload = function () {
+    console.log("Esta cargado el HTML");
+}
+ 
+let cantidadInput = document.querySelector("#cantidad");
+let colorInput = document.querySelector("#color");
 
-sumaVar = var1 + var2
+const precio = 400000;
 
-alert("El resultado es: " + sumaVar)
+// Funcion Calcular
+function calcular() {
+    console.log("Calculando");
+
+    //Calculo
+    let calculoTotal = Number(cantidadInput.value) * Number(precio);
+
+    //Conversion a Clp
+    let myNumeral = numeral (calculoTotal);
+    let currencyString = myNumeral.format('$ 0,0');
+
+    //Carga a Id desde Variables
+    document.querySelector("#cantidadTotal").innerHTML = cantidadInput.value;
+
+    document.querySelector("#valorTotal").innerHTML = currencyString;
+
+    let circulo = document.querySelector("#circuloColor");
+    circulo.style.backgroundColor = colorInput.value;
+}
+
+// Doy una accion al evento click
+let botonCalcular = document.querySelector("#btnCalcular");
+botonCalcular.addEventListener("click", calcular);
