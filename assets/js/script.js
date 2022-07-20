77b601ce3b8d38fbbc67f795d687e9c28921a9e0
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-07-20 18:23:28 -0400

    Fix : se corrige script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,16 @@
-// Verifica que la pagina cargue
-
-window.onload = function () {
-    console.log("Esta cargado el HTML");
-}
- 
+window.onload = function () { console.log("Esta cargado el HTML"); }
 let cantidadInput = document.querySelector("#cantidad");
 let colorInput = document.querySelector("#color");
-
 const precio = 400000;
-
-// Funcion Calcular
 function calcular() {
     console.log("Calculando");
-
-    //Calculo
     let calculoTotal = Number(cantidadInput.value) * Number(precio);
-
-    //Conversion a Clp
     let myNumeral = numeral(calculoTotal);
     let currencyString = myNumeral.format('$ 0,0');
-
-    //Carga a Id desde Variables
     document.querySelector("#cantidadTotal").innerHTML = cantidadInput.value;
-
     document.querySelector("#valorTotal").innerHTML = currencyString;
-
     let circulo = document.querySelector("#circuloColor");
     circulo.style.backgroundColor = colorInput.value;
 }
-
-// Doy una accion al evento click
 let botonCalcular = document.querySelector("#btnCalcular");
 botonCalcular.addEventListener("click", calcular);
